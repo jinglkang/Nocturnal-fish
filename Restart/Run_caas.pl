668318217e59b12b47a9da67e85afec9f967c396
Kang Jingliang <45533802+jinglkang@users.noreply.github.com>
2026-09-17 11:18:34 +0800

    Create Run_caas.pl

---
+++ Restart/Run_caas.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Getopt::Long;
+use Parallel::ForkManager;
+
+# hyphy /lustre1/g/sbs_schunter/Kang/Ldim_revision/BUSTED-MH.bf --alignment final_alignment.fa --tree /lustre1/g/sbs_schunter/Kang/Ldim_revision/spe_hyphy.tre --branches Foreground
+my $list=$ARGV[0]; # The list
+my $outd="CAAS";
+unless (-d $outd) {
+    mkdir $outd;
+}
+
+# ct discovery -a final_alignment_pep.fa -t ../config.tab -o discovery.output --fmt fasta
+
+my @cmds;
+open LIST, $list or die "can not open $list\n";
+while (<LIST>) {
+    chomp;
+    my @a=split;
+    my $orth=$a[0];
+    my $alig="$orth/final_alignment_pep.fa"; # the alignment
+    my $outl="$outd/$orth"."_caas.txt";
+    my $cmd ="ct discovery -a final_alignment_pep.fa -t ../config.tab -o $outl --fmt fasta";
+#   print "$cmd\n";
+    push @cmds, $cmd;
+}
+
+my $manager = new Parallel::ForkManager(80);
+foreach my $cmd (@cmds) {
+    $manager->start and next;
+    system($cmd);
+    $manager->finish;
+}
+$manager -> wait_all_children;
